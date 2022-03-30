568a55890cdc3002e5d8f305ccf70a0c8909ab92
Lucsy3012 <lucas.dietrich@gmx.de>
2022-03-30 21:55:24 +0200

    Legal Notice, Dynamic BG

--- locales/pages/legal.js
+++ locales/pages/legal.js
@@ -1,8 +1,87 @@
 export default {
-  "de": {
-    "title": "Impressum",
+  de: {
+    title: "Impressum",
+    address: {
+      title: "Angaben gemäß § 5 TMG",
+      name: "Lucas Dietrich",
+      street: "Zum Erlenbusch 21",
+      city: "Münster",
+      zip: "48167",
+    },
+    representative: {
+      title: "Vertreten durch:",
+      name: "Lucas Dietrich",
+    },
+    contact: {
+      phone: {
+        label: "Telefon",
+        value: "0151/15802400",
+      },
+      email: {
+        label: "E-Mail",
+        value: "lucas@lucasdietrich.de",
+      },
+    },
+    disclaimers: [
+      {
+        title: "Haftungsausschluss",
+        desc: "Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.",
+      },
+      {
+        title: "Haftung für Links",
+        desc: "Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.",
+      },
+      {
+        title: "Urheberrecht",
+        desc: "Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.",
+      },
+    ],
+    createdBy: {
+      desc: "Website Impressum von",
+      href: "https://impressum-generator.de"
+    }
   },
-  "en": {
-    "title": "Legal notice",
+  // todo translate
+  en: {
+    title: "Legal notice",
+    address: {
+      title: "Angaben gemäß § 5 TMG",
+      name: "Lucas Dietrich",
+      street: "Zum Erlenbusch 21",
+      city: "Münster",
+      zip: "48167",
+    },
+    representative: {
+      title: "Vertreten durch:",
+      name: "Lucas Dietrich",
+    },
+    contact: {
+      phone: {
+        label: "Telefon",
+        value: "0151/15802400",
+      },
+      email: {
+        label: "E-Mail",
+        value: "lucas@lucasdietrich.de",
+      },
+    },
+    disclaimers: [
+      {
+        title: "Haftungsausschluss",
+        desc: "Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.",
+      },
+      {
+        title: "Haftung für Links",
+        desc: "Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.",
+      },
+      {
+        title: "Urheberrecht",
+        desc: "Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.",
+      },
+    ],
+    createdBy: {
+      desc: "Legal notice generated by",
+      href: "https://impressum-generator.de"
+    }
   }
 }
--- pages/legal.vue
+++ pages/legal.vue
@@ -1,22 +1,46 @@
 <template>
   <div class="container">
-    <section class="section--xl">
-      <div class="inner">
-        <div class="row">
-          <div class="col col-12">
-            <div class="headline tb5 tb6-m tb7-xl text--center">
-              <span class="secondary">{{ $t('legal.title') }}</span>
-              <h1 class="primary">{{ $t('legal.title') }}</h1>
-            </div>
+    <section class="dynamic-bg--container pb0">
+      <div class="row dynamic-bg--offset--33">
+        <div class="col col-12">
+          <div class="headline tb5 tb6-m tb7-xl text--center">
+            <span class="secondary">{{ t.title }}</span>
+            <h1 class="primary">{{ t.title }}</h1>
           </div>
         </div>
       </div>
+      <section class="section--xl dynamic-bg dynamic-bg--yellow">
+        <div class="background"></div>
+        <div class="inner">
+          <div class="row justify-content--center">
+            <div class="col col-12 col-s-11">
+              <h2 class="tf1 tf2-s tf3-l">{{ t.address.title }}</h2>
+              <p class="tm-1 tm-s mt2">
+                {{ t.address.name }}<br/>
+                {{ t.address.street }}<br/>
+                {{ t.address.city }} {{ t.address.zip }}<br/>
+              </p>
+            </div>
+            <div class="col col-12 col-s-11">
+              <h2 class="tf1 tf2-s tf3-l">{{ t.representative.title }}</h2>
+              <p class="tm-1 tm-s mt2">
+                {{ t.representative.name }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </section>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: "Legal notice",
+  computed: {
+    t() {
+      return this.$t('legal')
+    }
+  }
 }
 </script>
