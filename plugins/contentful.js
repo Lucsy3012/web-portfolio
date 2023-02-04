9349fcec3526c8ea72695db15f3b0ac3d4a38221
Lucsy3012 <lucas.dietrich@gmx.de>
2023-02-04 17:09:04 +0100

    * Testing for Deployment

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,5 +1,9 @@
 const contentful = require('contentful')
 
+console.log('process', process)
+console.log('process.env', process.env)
+console.log('CONTENTFUL_DELIVERY_ACCESS_TOKEN', process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN)
+
 const client = contentful.createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
